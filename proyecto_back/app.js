c22855b0541e27ac98e3fa095be709db1b2f6cdb
EDER LEANDRO MELO RODRIGUEZ <eder.melo@MC.LOCAL>
2022-10-29 12:53:36 -0500

    Agregue productos y autenticacion de usuarios

--- proyecto_back/app.js
+++ proyecto_back/app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var database = require('./config/database')
+var database = require('./config/database');
+var auth = require('./auth/main_auth');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var empleadosRouter = require('./routes/empleados.router');
 var noviosRouter = require('./routes/novios.router');
+var usuariosRouter = require('./routes/usuarios.router');
 
 var app = express();
 
@@ -18,13 +18,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//mongo connection
+//Mongo connection
 database.mongoConnect();
+app.use('/usuarios', usuariosRouter);
+app.use(auth);
 
-//router
+//Router
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/empleados', empleadosRouter);
 app.use('/novios', noviosRouter);
 
